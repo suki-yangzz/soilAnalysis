bc7dd8534cf3aca3a6fb492f0befb68b72c9a91e
zhenzyang <zhenzyang@ebay.com>
2021-01-20 16:42:23 +0800

    initial commit

---
+++ src/main/java/com/example/soilanalysis/model/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.example.soilanalysis.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+    List<Group> findAllByUserId(String id);
+}
